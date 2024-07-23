442b7de6805ca3c91cb05298e96bcfa6682bad11
anhlt <anhlt@twendeesoft.com>
2024-07-23 14:39:38 +0700

    init project

---
+++ src/test/java/twendee/fantasy/be/FantasySqliteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package twendee.fantasy.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasySqliteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
